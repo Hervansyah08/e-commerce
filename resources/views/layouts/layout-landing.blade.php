4b416c9a0fd1b802ce63429c1a495c4dda71f5d0
hervansyah08 <hervansyahakbar@gmail.com>
2025-05-14 21:12:26 +0700

    instal laravel spatie

--- resources/views/layouts/layout-landing.blade.php
+++ resources/views/layouts/layout-landing.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>RD Iphone House</title>
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
 </head>
@@ -14,7 +14,6 @@
     @include('components.navbar')
 
     @include('components.hero')
-
 
 
 </body>
